b44e600b77fb0468aa8716b990f9b159c32625f9
sinan akyol <110792519+sinanakyol@users.noreply.github.com>
2022-11-02 00:38:07 +0300

    music.js changed

--- music.js
+++ music.js
@@ -12,7 +12,47 @@ class Music {
 }
 
 const musicList = [
-  new Music("Boşver", "Nilüfer", "1.jpeg", "1.mp3"),
-  new Music("Bu da Geçer mi Sevgilim", "Yalın", "2.jpeg", "2.mp3"),
-  new Music("Aramızda Uçurumlar", "Suat Suna", "3.jpeg", "3.mp3"),
+  new Music(
+    "Ara",
+    "Zeynep Bastik",
+    "1.jpg",
+    "mp3indirdur-Zeynep-Bastik-Ara.mp3"
+  ),
+  new Music(
+    "Küçük Bir Yol",
+    "Hadise",
+    "2.jpg",
+    "mp3indirdur-Hadise-Kucuk-Bir-Yol.mp3"
+  ),
+  new Music(
+    "Aşk Dediğin",
+    "Hadise",
+    "3.jpg",
+    "mp3indirdur-Hadise-Ask-Dedigin.mp3"
+  ),
+  new Music(
+    "Bu da Geçer mi Sevgilim",
+    "Yalın",
+    "4.jpg",
+    "mp3indirdur-Yalin-Bu-Da-Gecer-Mi-Sevgilim.mp3"
+  ),
+  new Music("Yaz Gülü", "Yalın", "5.jpg", "mp3indirdur-Yalin-Yaz-Gulu.mp3"),
+  new Music(
+    "Belki",
+    "Dedublüman",
+    "6.jpg",
+    "mp3indirdur-Dedubluman-Belki-(Akustik)-.mp3"
+  ),
+  new Music(
+    "Karakol",
+    "Mabel Matiz",
+    "7.jpg",
+    "mp3indirdur-Mabel-Matiz-Karakol.mp3"
+  ),
+  new Music(
+    "Gelsin Hayat Bildiği Gibi",
+    "Ceza",
+    "8.jpg",
+    "mp3indirdur-Ceza-Gelsin-Hayat-Bildigi-Gibi-feat-Sezen-Aksu.mp3"
+  ),
 ];
